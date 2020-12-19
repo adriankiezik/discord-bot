029efa8d359e8344320f86750c871990bb64132d
Adrian Kiezik <kiezikadrian@gmail.com>
2020-12-19 12:00:05 +0100

    Delete test.js
    Not supposed to be here. Just test file.

--- test.js
+++
@@ -1,9 +0,0 @@
-const { get } = require("mongoose")
-
-getSilver(1123)
-
-function getSilver(currency) {
-    var silver = Math.floor(currency/100)
-    var bronze = currency % 100
-    console.log(silver, bronze)
-}
